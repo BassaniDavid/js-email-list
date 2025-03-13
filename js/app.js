0d2612ac5ce10820be3ab362cc2a3068d9d9c96c
BassaniDavid <davbassan98@gmail.com>
2025-03-13 14:08:02 +0100

    feat: test generate one list element in console

--- js/app.js
+++ js/app.js
@@ -3,8 +3,21 @@
 fetch("https://flynn.boolean.careers/exercises/api/random/mail")
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        console.log(data.response);
+        createHtml (data.response)
+        console.log(items)
     })
     .catch(error => {
         console.error(error);
     });
+
+
+    const emailList = document.getElementById("email-list")
+    console.log(emailList)
+    let items = ''
+    // funzione per inserire html
+
+    function createHtml (dataEmail) {
+        items += `<li>${dataEmail}</li>`
+    }
+
