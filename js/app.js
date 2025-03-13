dc826e1052f1f635b1b36ffd5b59ad5dd3fffe3e
BassaniDavid <davbassan98@gmail.com>
2025-03-13 13:58:37 +0100

    feat: fetch test

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+// fetch per fare una richiesta all API di boolean
+fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
